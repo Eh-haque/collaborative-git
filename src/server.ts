b6d089ce229e75251eabfc3773149d566e6c6d1b
Eh-haque <ehtisamul@gmail.com>
2025-04-29 23:58:15 +0600

    feat: login fucntionality added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import login from "./auth/loing";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.post("/login", login);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
